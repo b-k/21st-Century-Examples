59c23a872af7612d1f9f43e41e8a7d9f716ae2fd
Ben <ben@klemens.org>
2014-04-11 17:43:34 -0400

    Minor fixes and rewordings

--- strtod.c
+++ strtod.c
@@ -8,5 +8,5 @@ int main(int argc, char **argv){
     double in = strtod(argv[1], &end);
     Stopif(*end, return 2, "I couldn't parse '%s' to a number. "
                 "I had trouble with '%s'.", argv[1], end);
-    printf("The square of %g is %g\n", argv[1], pow(in, 2));
+    printf("The square of %s is %g\n", argv[1], pow(in, 2));
 }
